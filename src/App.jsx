e50e0a83fafde096f849dd2e3ea9f7aed89955cd
ReneRisa <risael31@gmail.com>
2023-11-29 19:04:12 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+//Components
+import NavBarComponent from "./components/NavBarComponent/NavBarComponent";
+//Styles
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <NavBarComponent />
+    </div>
+  );
+}
+
+export default App;
