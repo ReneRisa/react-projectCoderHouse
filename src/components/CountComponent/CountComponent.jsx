8f46c38e9a2050daf6b08cb2ece5446bc22e6ebd
ReneRisa <risael31@gmail.com>
2023-11-29 20:30:46 -0500

    creacion de countcomponent y useEffect

---
+++ src/components/CountComponent/CountComponent.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+const CountComponent = () => {
+  const [count, setCount] = useState(0);
+  const incrementCount = () => setCount(count + 1);
+  const decrementCount = () => setCount(count - 1);
+  //se va a ejectuar siempre que el componente se monte
+  //useEffect es una funcion que se ejecuta cada vez que se renderiza el componente
+  //useEffect sin dependecias.
+  useEffect(() => {
+    console.log(count);
+    console.log("se ejecuta el useEffect sin dependencias");
+  });
+  // se va a ejecutar la primera vez que el componente se monte
+  //useEffect con array de dependecias vacias
+  useEffect(() => {
+    console.log(count);
+    console.log(
+      "se ejecuta una unica vez por que tiene dependencias y se renderiza una unica vez"
+    );
+  }, []);
+
+  //useEffect con array de dependecias.
+  useEffect(() => {
+    console.log(count);
+    console.log("se ejecuta el useEffect con dependencias");
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={incrementCount}>Increment</button>
+      <h1>{count}</h1>
+      <button onClick={decrementCount}>Decrement</button>
+    </div>
+  );
+  return <div></div>;
+};
+
+export default CountComponent;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBarComponent from "../components/NavBarComponent/NavBarComponent";
+import CountComponent from "../components/CountComponent/CountComponent";
 
 const MainLayout = ({ children }) => {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <NavBarComponent />
+      <CountComponent />
       {children}
     </div>
   );
