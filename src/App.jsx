f8dec0986a42c7d4cf44d06f7245e7973ade1c3f
ReneRisa <risael31@gmail.com>
2023-11-29 19:47:43 -0500

    Creating a Main Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import { useState } from "react";
 
 //Components
 import NavBarComponent from "./components/NavBarComponent/NavBarComponent";
+import MainLayout from "./layouts/MainLayout";
 //Styles
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div>
-      <NavBarComponent />
-    </div>
-  );
+  return <MainLayout></MainLayout>;
 }
 
 export default App;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBarComponent from "../components/NavBarComponent/NavBarComponent";
+
+const MainLayout = ({ children }) => {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <NavBarComponent />
+      {children}
+    </div>
+  );
+};
+
+export default MainLayout;
